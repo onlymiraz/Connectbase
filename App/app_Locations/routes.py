5fa3c80f475633eb80a13bdb784b16ff3312a7b5
onlymiraz <34901980+onlymiraz@users.noreply.github.com>
2025-05-27 14:07:04 -0500

    Delete App/app_Locations directory

--- App/app_Locations/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template
-
-def all_locations_map():
-    return render_template('app_Locations/all_locations_map.html')
-
-def all_locations_map_v2():
-    return render_template('app_Locations/all_locations_map_v2.html')
